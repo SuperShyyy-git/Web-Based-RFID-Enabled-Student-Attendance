ab67638e272730b9c59e6107bdc4dd9216a9c284
noellao25 <noel_lao25@yahoo.com>
2026-01-05 11:01:06 +0800

    Fix HTTPS mixed content by forcing URL scheme in production

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    App\Providers\AppServiceProvider::class,
     App\Providers\AccountServiceProvider::class,
     App\Providers\AssignmentServiceProvider::class,
     App\Providers\DepartmentProvider::class,
